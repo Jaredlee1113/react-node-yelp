e901bfa16325c8eba3dd16826db5566f314feb66
Jaredlee1113 <Jared.Lee.1113@gmail.com>
2025-07-04 06:49:09 +0800

    feat: fix getRestaurantById test error and complete success test cases for restaurants APIs

--- server/tests/restaurants.controller.test.js
+++ server/tests/restaurants.controller.test.js
@@ -9,14 +9,13 @@ import {
 } from "../controllers/restaurants.controller.js";
 
 describe("Restaurants Controllers", () => {
-    const res = {
-        json: vi.fn(),
-        status: vi.fn().mockReturnThis(),
-    };
+    let res;
     beforeEach(() => {
         vi.restoreAllMocks();
-        res.json.mockReset();
-        res.status.mockReset();
+        res = {
+            json: vi.fn(),
+            status: vi.fn().mockReturnThis(),
+        };
     });
 
     it("Get all restaunrants - success", async () => {
@@ -34,30 +33,76 @@ describe("Restaurants Controllers", () => {
     });
 
     it("Get restaurant by id - success", async () => {
+        const restaurants = [{ id: 1, name: "Mock Restaurant", price_range: "Mock Price Range 3" }];
+        const reviews = [
+            { id: 1, name: "Mock Name", restaurant_id: 1, review: "Tasty!", rating: 4 },
+        ];
+        const req = { params: { id: 1 } };
         vi.spyOn(db.default, "query")
             .mockResolvedValueOnce({
                 rowCount: 1,
-                rows: [{ id: 1, name: "Mock Restaurant", price_range: "Mock Price Range 3" }],
+                rows: restaurants,
             })
             .mockResolvedValueOnce({
-                rows: [{ id: 1, name: "Mock Name", restaurant_id: 1, review: "Tasty!", rating: 4 }],
+                rows: reviews,
             });
-
-        const req = { params: { id: 1 } };
 
         await getRestaurantById(req, res);
 
-        expect(res.status).toHaveBeenCalledWith(500);
-
+        expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({
             status: "success",
             count: 1,
             data: {
-                restaurant: { id: 1, name: "Mock Restaurant", price_range: "Mock Price Range 3" },
-                reviews: [
-                    { id: 1, name: "Mock Name", restaurant_id: 1, review: "Tasty!", rating: 4 },
-                ],
+                restaurant: restaurants[0],
+                reviews: reviews,
             },
+        });
+    });
+
+    it("Create restaurant - success", async () => {
+        const newRestaurant = {
+            name: "mock name",
+            location: "mock location",
+            price_range: 3,
+        };
+        vi.spyOn(db.default, "query").mockResolvedValueOnce({
+            rows: [newRestaurant],
+        });
+        const req = { body: newRestaurant };
+        await createRestaurant(req, res);
+        res.status.mockResolvedValueOnce(200);
+        res.status.mockResolvedValueOnce({
+            status: "success",
+            data: newRestaurant,
+        });
+    });
+
+    it("Update restaurant - success", async () => {
+        const updateRestaurantData = {
+            name: "mock name update",
+            location: "mock location update",
+            price_range: 4,
+        };
+        vi.spyOn(db.default, "query").mockResolvedValueOnce({
+            rows: [updateRestaurantData],
+        });
+        const req = { body: updateRestaurantData, params: { id: 5 } };
+        await updateRestaurant(req, res);
+        res.status.mockResolvedValueOnce(200);
+        res.status.mockResolvedValueOnce({
+            status: "success",
+            data: updateRestaurantData,
+        });
+    });
+
+    it("Delete restaurant - success", async () => {
+        vi.spyOn(db.default, "query").mockResolvedValueOnce({});
+        const req = { params: { id: 5 } };
+        await deleteRestaurant(req, res);
+        res.status.mockResolvedValueOnce(200);
+        res.status.mockResolvedValueOnce({
+            status: "success",
         });
     });
 });
